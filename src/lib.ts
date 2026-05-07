6c80dcc24fdd4bedf96c042ab2689f64d64decb2
A70III <surapitch2011@gmail.com>
2026-05-07 13:28:49 +0700

    update lib and dependencies

--- src/lib.ts
+++ src/lib.ts
@@ -57,3 +57,80 @@ export { compose } from "./middleware";
 // ==================== Plugins ====================
 export { cors } from "./plugins";
 export type { CorsOptions } from "./plugins";
+
+// ==================== Auth ====================
+export { Auth, createAuth, type AuthOptions } from "./auth/auth";
+export {
+  createAccessToken,
+  createRefreshToken,
+  createTokenPair,
+  verifyToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+  decodeToken,
+  setJWTSecret,
+  getJWTSecret,
+} from "./auth/jwt";
+export type {
+  TokenPayload,
+  JWTConfig,
+  TokenPair,
+} from "./auth/jwt";
+
+export {
+  createSession,
+  getSession,
+  refreshSession,
+  updateSession,
+  destroySession,
+  destroyAllUserSessions,
+  getUserSessions,
+  cleanupExpiredSessions,
+  setSessionConfig,
+  getSessionConfig,
+} from "./auth/session";
+export type {
+  Session,
+  SessionData,
+  SessionConfig,
+} from "./auth/session";
+
+export {
+  defineRole,
+  getRole,
+  getAllRoles,
+  setUserRoles,
+  getUserRoleNames,
+  getUserPermissions,
+  hasPermission,
+  hasAnyPermission,
+  hasAllPermissions,
+  hasRole,
+  hasAnyRole,
+  removeUserRoles,
+} from "./auth/rbac";
+export type { Permission, Role, UserRoles } from "./auth/rbac";
+
+export {
+  setOAuthConfig,
+  getOAuthConfig,
+  getAuthorizationUrl,
+  exchangeCodeForToken,
+  getUserInfo,
+  refreshOAuthToken,
+  generateState,
+} from "./auth/oauth2";
+export type {
+  OAuthProvider,
+  OAuthConfig,
+  OAuthState,
+  OAuthTokens,
+  OAuthUserInfo,
+} from "./auth/oauth2";
+
+// ==================== Testing ====================
+export { TestClient, TestApp, createApp, expect, expectStatus, expectBody } from "./test/test";
+export { mocking } from "./test/test";
+export type { TestRequest, TestResponse, AssertionResult } from "./test/test";
+export { spy, stub, MockDatabase, MockResponse, mockRequest, createMockContext } from "./test/mocking";
+export type { MockFunction } from "./test/mocking";
